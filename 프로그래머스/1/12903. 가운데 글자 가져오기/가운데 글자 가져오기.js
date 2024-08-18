e6f6404db70cad2776e364bd0caf52d3d23b567d
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2024-08-18 14:09:41 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 2.53 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let answer = '';
+    if(s.length%2 ==0)
+    {
+        answer = s[s.length/2 -1] + s[s.length/2];
+    }
+    else
+    {
+        console.log(Math.floor(s.length/2));
+        answer = s[Math.floor(s.length/2)];
+    }
+    return answer;
+}
