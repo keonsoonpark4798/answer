1fe3cd4ed6010f657f8badee712875b1088d4114
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2024-08-18 14:04:19 +0900

    [level 0] Title: 배열의 평균값, Time: 0.83 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        
+        // for(int i =0; i<numbers.Length;i++)
+        // {
+        //     answer = answer + numbers[i];
+        // }
+        // answer = answer / numbers.Length;
+        return answer = numbers.Average();
+    }
+}
