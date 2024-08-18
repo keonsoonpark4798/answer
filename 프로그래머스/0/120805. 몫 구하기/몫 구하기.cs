4fbfbbb5327d58a0985822d081b18623f737ee16
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2024-08-18 14:04:32 +0900

    [level 0] Title: 몫 구하기, Time: 0.40 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        num1 = Math.Clamp(num1,0,100);
+        num2 = Math.Clamp(num2,0,100);
+        return answer=num1/num2;
+    }
+}
