4a7e66652eda066bc9a80cc43840eaaf3c8bed6d
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2024-08-18 14:13:22 +0900

    [level 1] Title: 시저 암호, Time: 3.37 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,26 @@
+function solution(s, n) {
+    let answer = '';
+    for(let i =0; i<s.length;i++)
+    {
+        let ascii = s.charCodeAt(i);
+        if(ascii >= 65 && ascii <=90)
+        {
+            ascii +=n;
+            if(ascii > 90)
+            {
+                ascii -= 26;
+            }
+        }
+        else if(ascii >=97 && ascii <=122)
+        {
+            ascii +=n;
+            if(ascii > 122)
+            {
+                ascii -= 26;
+            }
+        }
+        
+        answer += String.fromCharCode(ascii);
+    }
+    return answer;
+}
