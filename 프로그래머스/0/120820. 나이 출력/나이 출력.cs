b6fbc640c65849102811f773515d82dd98e2c3a4
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2024-08-18 14:03:46 +0900

    [level 0] Title: 나이 출력, Time: 0.34 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        age = Math.Clamp(age,0,120);
+        return answer = 2022-age+1;
+    }
+}
