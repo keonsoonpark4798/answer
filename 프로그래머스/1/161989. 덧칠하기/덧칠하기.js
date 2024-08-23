ec666366bb5babb771f27c530637ab04cbff63f0
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-08-23 09:52:27 +0900

    Update 덧칠하기.js

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,7 @@
 function solution(n, m, section) {
     let answer = 1;
-    // let maxlength = section[section.length-1] - section[0] + 1;
-    // answer = Math.floor(maxlength / m);
-    // if(maxlength%m != 0)
-    // {
-    //     answer++;
-    // }
     let count = m-1;
+    
     for(let i = 1;i<section.length;i++)
     {
         let interval = section[i] - section[i-1];
