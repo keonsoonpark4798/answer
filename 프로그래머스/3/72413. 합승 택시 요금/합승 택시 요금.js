f958fc7512fd1d9bc0cba87527df3a3fb5ef48f7
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2025-03-19 10:50:43 +0900

    [level 3] Title: 합승 택시 요금, Time: 61.13 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -0,0 +1,101 @@
+function solution(n, s, a, b, fares) {
+    var answer = Infinity;
+    var graph = createGraph(fares);
+    var sStart = dijkstra(graph,s);
+    var aStart = dijkstra(graph,a);
+    var bStart = dijkstra(graph,b);
+    
+    var final = {};
+    const allNodes = new Set([...Object.keys(sStart), ...Object.keys(aStart), ...Object.keys(bStart)]);
+    allNodes.forEach(node =>{
+        let totalWeight = 0;
+        
+        if(sStart[node] !== null) totalWeight += sStart[node];
+        if(aStart[node] !== null) totalWeight += aStart[node];
+        if(bStart[node] !== null) totalWeight += bStart[node];
+        
+        if(totalWeight > 0){
+            final[node] = totalWeight;        
+        }
+    })
+    for (let node in final) {
+        if (final[node] < answer) {
+            answer = final[node];
+        }
+    }
+    return answer;
+}
+function dijkstra(graph, start){
+    let distances = {};
+    let visited = {};
+    let nodes = new priorityQueue();
+    
+    for(let vertex in graph){
+        distances[vertex] = Infinity;
+         nodes.enqueue(vertex, Infinity);
+    }
+    
+    distances[start] = 0;
+    nodes.enqueue(start, 0);
+    
+    while(!nodes.isEmpty()){
+        let currentVertex = nodes.dequeue().element;
+        visited[currentVertex] = true;
+        
+        for(let neighbor in graph[currentVertex]){
+            if(visited[neighbor]) continue;
+            
+            let distance = graph[currentVertex][neighbor];
+            let newDist = distances[currentVertex] + distance;
+            
+            if(newDist < distances[neighbor]){
+                distances[neighbor] = newDist;
+                nodes.enqueue(neighbor, newDist);
+            }
+        }
+    }
+    return distances;
+}
+function createGraph(wires) {
+	let graph = {};
+
+    wires.forEach(([start, end, weight]) => {
+    if (!graph[start]) graph[start] = {};
+    if (!graph[end]) graph[end] = {};
+
+    graph[start][end] = weight;
+    graph[end][start] = weight;
+    });
+
+	return graph;
+}
+
+class priorityQueue{
+    constructor(){
+        this.items = [];
+    }
+    enqueue(element, priority){
+        let queueElement = {element, priority};
+        let added = false;
+        
+        for(let i = 0; i< this.items.length;i++){
+            if(queueElement.priority < this.items[i].priority){
+                this.items.splice(i, 0, queueElement);
+                added = true;
+                break;
+            }
+        }
+        if(!added){
+            this.items.push(queueElement);
+        }
+    }
+    
+    dequeue(){
+        return this.items.shift();
+    }
+    isEmpty(){
+        return this.items.length === 0;
+    }
+}
+
+
