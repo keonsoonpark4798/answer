e40ae0fc26467dc2c6c43e6796127de8b6eb490d
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2025-03-20 15:16:13 +0900

    [level 3] Title: 등굣길, Time: 3.53 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.4 MB, 시간: 5.10 ms
+메모리: 36.5 MB, 시간: 3.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 15:07:18
+2025년 03월 20일 15:16:12
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,35 +1,21 @@
 function solution(m, n, puddles) {
-    var visited = Array(n).fill(1).map(row => Array(m).fill(0));
+    var visited = Array.from(Array(n),() => Array(m).fill(0));
+    puddles.forEach(([x,y]) =>{visited[y-1][x-1] = -1;})
     visited[0][0] = 1;
-    puddles.forEach(puddle =>{
-        visited[puddle[1]-1][puddle[0]-1] = -1;
-    })
+    
     if(visited[n-2][m-1] == -1 && visited[n-1][m-2] == -1) return 0;
     
-    for(let i = 0;i<=n-1;i++){
-        for(let j = 0; j<=m-1;j++){
+    for(let i = 0;i < n;i++){
+        for(let j = 0; j < m;j++){
             if(i == 0 && j == 0) continue;
             if(visited[i][j] == -1) continue;
-            if(i == 0) {
-                if(visited[i][j-1] == 1){
-                    visited[i][j] = 1;
-                }else{
-                    visited[i][j] = -1;
-                }
-            }else if(j == 0){
-                if(visited[i-1][j] == 1){
-                    visited[i][j] = 1;
-                }else{
-                    visited[i][j] = -1;
-                }
-            }
-            else{
-                let value = 0;
-                if(visited[i-1][j] !== -1) value += visited[i-1][j];
-                if(visited[i][j-1] !== -1) value += visited[i][j-1];
-                visited[i][j] = value % 1000000007;
-            }
+            
+            let value = 0;
+            if(i-1>=0 && visited[i-1][j] !== -1) value += visited[i-1][j];
+            if(j-1>=0 && visited[i][j-1] !== -1) value += visited[i][j-1];
+            visited[i][j] = value % 1000000007;
+
         }
     }
-    return visited[n-1][m-1];
+    return visited[n-1][m-1] % 1000000007;
 }
