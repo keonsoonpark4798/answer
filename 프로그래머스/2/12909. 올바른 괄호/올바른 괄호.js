544053d8024fc8399a45cdc1e1fc748fa0a9ea3c
keonsoonpark4798 <75961047+keonsoonpark4798@users.noreply.github.com>
2025-03-27 14:27:14 +0900

    Update 올바른 괄호.js 좀 더 효율성 있게
    for문 in이 아닌 of로 char 직접 가져오기
    ===으로 좀 더 엄격한 비교
    선입후출 방식이기때문에 queue가 아닌 stack 명명

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,22 +1,39 @@
-function solution(s){
-    var answer = true;
-    let queue = [];
+// function solution(s){
+//     var answer = true;
+//     let queue = [];
     
-    for(str in s){
-        if(s[str] == "("){
-            queue.push(s[str]);
-        }else{
-            if(queue.length == 0){
-                answer = false;
-                break;
-            }else{
-                queue.pop();
+//     for(str in s){
+//         if(s[str] == "("){
+//             queue.push(s[str]);
+//         }else{
+//             if(queue.length == 0){
+//                 answer = false;
+//                 break;
+//             }else{
+//                 queue.pop();
+//             }
+//         }
+//     }
+//     if(queue.length != 0){
+//         answer = false;
+//     }
+
+//     return answer;
+// }
+
+function solution(s) {
+    let stack = [];
+
+    for (let char of s) {
+        if (char === "(") {
+            stack.push(char);
+        } else {
+            if (stack.length === 0) {
+                return false;
             }
+            stack.pop();
         }
     }
-    if(queue.length != 0){
-        answer = false;
-    }
 
-    return answer;
+    return stack.length === 0;
 }
